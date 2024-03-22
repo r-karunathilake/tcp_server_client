fdc7acd5ba40c91030d66a1f17d2cb178efb6e25
Ravindu Karunathilake <karunath@ualberta.ca>
2024-03-22 12:22:24 -0600

    Created 'server.c' file.

---
+++ server.c
@@ -0,0 +1,23 @@
+/* 
+    This is a simple TCP server written in the C programming language.
+    The program is written and tested on Ubuntu.
+            
+        Ubuntu version:         22.04.3 LTS
+        lippcap version:        1.10.1
+        C/C++ compiler version: GCC 11.4.0
+        Linux kernel version:   5.15.90.1-microsoft-standard-WSL2
+        
+    Author: Ravindu Karunathilake
+    Date:   2024/03/22
+*/
+
+
+// Function prototypes 
+
+
+
+
+int main(int argc, char* argv[]){
+    
+    return 0;
+}
